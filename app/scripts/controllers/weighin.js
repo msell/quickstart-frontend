148f72f0791bf1fdf758952552a62b76bfc776e1
Matt Sell <mwsell@gmail.com>
2015-01-01 18:58:03 -0600

    add mock data to weighin view

--- app/scripts/controllers/weighin.js
+++ app/scripts/controllers/weighin.js
@@ -14,9 +14,17 @@ angular.module('meanApp')
             $state.go('login');
         }
 
-        $http.get(configuration.apiUrl + 'weighIn').success(function (weighIns) {
-            $scope.weighIns = weighIns;
-        }).error(function (err) {
-            alert('warning', 'unable to get weighins', err.message);
-        })
+    $scope.weighIns = [{'weight':250, 'date':'2014-01-01T13:15:34.441Z'},
+                      {'weight':242, 'date':'2014-01-08T13:15:34.441Z'},
+                       {'weight':240, 'date':'2014-01-16T13:15:34.441Z'},
+                       {'weight':233, 'date':'2014-01-22T13:15:34.441Z'},
+                       {'weight':237, 'date':'2014-01-30T13:15:34.441Z'},
+                       {'weight':233, 'date':'2014-02-05T13:15:34.441Z'},
+                       {'weight':229, 'date':'2014-02-12T13:15:34.441Z'}                       
+                      ]
+//        $http.get(configuration.apiUrl + 'weighIn').success(function (weighIns) {
+//            $scope.weighIns = weighIns;
+//        }).error(function (err) {
+//            alert('warning', 'unable to get weighins', err.message);
+//        })
     });
