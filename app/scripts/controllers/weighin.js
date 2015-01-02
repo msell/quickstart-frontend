5404bf18b385e7d4e3d78ae4ad1007f0b24555e4
Matt Sell <mwsell@gmail.com>
2015-01-01 21:07:04 -0600

    tweek weighins

--- app/scripts/controllers/weighin.js
+++ app/scripts/controllers/weighin.js
@@ -8,23 +8,46 @@
  * Controller of the meanApp
  */
 angular.module('meanApp')
-    .controller('WeighinCtrl', function ($scope, $http, configuration, alert, $auth, $state) {    
-        
-    if (!$auth.isAuthenticated()) {            
+    .controller('WeighinCtrl', function ($scope, $http, configuration, alert, $auth, $state) {
+
+        if (!$auth.isAuthenticated()) {
             $state.go('login');
         }
 
-    $scope.weighIns = [{'weight':250, 'date':'2014-01-01T13:15:34.441Z'},
-                      {'weight':242, 'date':'2014-01-08T13:15:34.441Z'},
-                       {'weight':240, 'date':'2014-01-16T13:15:34.441Z'},
-                       {'weight':233, 'date':'2014-01-22T13:15:34.441Z'},
-                       {'weight':237, 'date':'2014-01-30T13:15:34.441Z'},
-                       {'weight':233, 'date':'2014-02-05T13:15:34.441Z'},
-                       {'weight':229, 'date':'2014-02-12T13:15:34.441Z'}                       
-                      ]
-//        $http.get(configuration.apiUrl + 'weighIn').success(function (weighIns) {
-//            $scope.weighIns = weighIns;
-//        }).error(function (err) {
-//            alert('warning', 'unable to get weighins', err.message);
-//        })
+        $scope.weighIns = [
+            {
+                'weight': 250,
+                'date': '2014-01-01'
+            },
+            {
+                'weight': 242,
+                'date': '2014-01-08'
+            },
+            {
+                'weight': 240,
+                'date': '2014-01-16'
+            },
+            {
+                'weight': 233,
+                'date': '2014-01-22'
+            },
+            {
+                'weight': 237,
+                'date': '2014-01-30'
+            },
+            {
+                'weight': 233,
+                'date': '2014-02-05'
+            },
+            {
+                'weight': 229,
+                'date': '2014-02-12'
+            }
+                      ];
+
+        //        $http.get(configuration.apiUrl + 'weighIn').success(function (weighIns) {
+        //            $scope.weighIns = weighIns;
+        //        }).error(function (err) {
+        //            alert('warning', 'unable to get weighins', err.message);
+        //        })
     });
