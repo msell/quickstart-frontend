c0cf9c6b37e514807076c3dfe255594275bbfcb7
Matt Sell <mwsell@gmail.com>
2015-01-03 15:40:41 -0600

    make ajax call

--- app/scripts/directives/setweightlossgoal.js
+++ app/scripts/directives/setweightlossgoal.js
@@ -7,7 +7,7 @@
  * # setWeightLossGoal
  */
 angular.module('meanApp')
-    .directive('setWeightLossGoal', function () {
+    .directive('setWeightLossGoal', function ($http, configuration) {
         return {            
             restrict: 'E',
             templateUrl: '../views/partials/setWeightLossGoal.html',
@@ -15,6 +15,18 @@ angular.module('meanApp')
             controller: function(){
                 this.currentDate = new Date();
                 this.endDate = new Date();
+                this.goalWeight = 0;
+                this.currentWeight = 0;
+                
+                this.create = function(){
+                    console.log('http post goal');
+                    $http.post(configuration.apiUrl + 'weightLossGoal'),{
+                        'startDate': this.currentDate,
+                        'endDate': this.endDate,
+                        'currentWeight': this.currentWeight,
+                        'goalWeight' : this.goalWeight
+                    }
+                };
             }
         }
     });
