b4afd53ed8450f8c0bb96c82cc21a1538615ce7c
Matt Sell <mwsell@gmail.com>
2015-01-02 23:28:12 -0600

    use controllerAs syntax on directive

--- app/scripts/directives/setweightlossgoal.js
+++ app/scripts/directives/setweightlossgoal.js
@@ -10,6 +10,11 @@ angular.module('meanApp')
     .directive('setWeightLossGoal', function () {
         return {            
             restrict: 'E',
-            templateUrl: '../views/partials/setWeightLossGoal.html'
+            templateUrl: '../views/partials/setWeightLossGoal.html',
+            controllerAs: 'goal',
+            controller: function(){
+                this.currentDate = new Date();
+                this.endDate = new Date();
+            }
         }
     });
