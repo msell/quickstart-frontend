b74fa0308b063647ab4104b455457edb29a7ce9b
Matt Sell <mwsell@gmail.com>
2015-01-05 22:17:46 -0600

    post goals

--- app/scripts/directives/setweightlossgoal.js
+++ app/scripts/directives/setweightlossgoal.js
@@ -8,26 +8,26 @@
  */
 angular.module('meanApp')
     .directive('setWeightLossGoal', function ($http, configuration) {
-        return {            
+        return {
             restrict: 'E',
             templateUrl: '../views/partials/setWeightLossGoal.html',
             controllerAs: 'goal',
-            controller: function(){
+            controller: function () {
                 this.currentDate = new Date();
                 this.endDate = new Date();
                 this.goalWeight = 0;
                 this.currentWeight = 0;
-                
-                this.create = function(){
+
+                this.create = function () {
+
                     var request = {
                         'startDate': this.currentDate,
                         'endDate': this.endDate,
                         'startWeight': this.currentWeight,
-                        'endWeight' : this.goalWeight
-                        //'user': 1
-                    }
+                        'endWeight': this.goalWeight
+                    };
                     console.log(request);
-                    $http.post(configuration.apiUrl + 'weightLossGoal',request)
+                    $http.post(configuration.apiUrl + 'weightLossGoal', request)
                 };
             }
         }
